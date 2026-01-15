1f7228fd5be9d1c648aa9d009d0a1395c4447195
chinturai <chintu.23ise@cambridge.edu.in>
2026-01-15 14:49:37 +0530

    Postgresql Database Setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import app from "./app.js";
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT http://localhost:${PORT} `);
+  console.log(`Listening on PORT http://localhost:${PORT} `);
 });
 
